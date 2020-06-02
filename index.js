afbaceadba3434e85d3398c9d3d634fd78ef18f6
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-01 19:49:18 -0700

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time) {
+  time = time.split(':')[0]
+  if (time < 12) {return "Good Morning"}
+  else if (time < 17) {return "Good Afternoon"}
+  else {return "Good Evening"}
+}
+
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message
+}
